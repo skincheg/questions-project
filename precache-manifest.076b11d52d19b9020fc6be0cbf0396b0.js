b90b4b83b6856b10a61b3527a74447dc1e3d5ba0
Skinxcheg <Skinxcheg@mail.ru>
2020-07-26 01:17:34 +0500

    Init

---
+++ precache-manifest.076b11d52d19b9020fc6be0cbf0396b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686c8315b84ecdb84ce3",
+    "url": "/question-project/css/app.f8d8539f.css"
+  },
+  {
+    "revision": "4f08fad59ecbba00fcb1ed3d8b8b3d83",
+    "url": "/question-project/index.html"
+  },
+  {
+    "revision": "686c8315b84ecdb84ce3",
+    "url": "/question-project/js/app.94c473f0.js"
+  },
+  {
+    "revision": "829bf88ec6c7b688883b",
+    "url": "/question-project/js/chunk-vendors.755a5d3d.js"
+  },
+  {
+    "revision": "b9296458bc236beec8c6285701cfba84",
+    "url": "/question-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/question-project/robots.txt"
+  }
+]);
